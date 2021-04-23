71e2179b7f917c3ff95b7baf83f6ab3ee6427f8d
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-04-23 17:30:46 -0300

    Removing setters and @NotNull annotations from CreateAuthorForm

--- src/main/java/br/com/zupacademy/gabriela/casadocodigo/Author/CreateAuthorForm.java
+++ src/main/java/br/com/zupacademy/gabriela/casadocodigo/Author/CreateAuthorForm.java
@@ -4,32 +4,26 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 public class CreateAuthorForm {
 
-    @NotNull @NotEmpty
+    public CreateAuthorForm(String name, String email, String description) {
+        this.name = name.trim();
+        this.email = email.trim().toLowerCase();
+        this.description = description.trim();
+    }
+
+    @NotEmpty
     private String name;
 
-    @NotNull @NotEmpty @Email
+    @NotEmpty
+    @Email
     private String email;
 
     @Length(max = 400)
     private String description;
 
-    public Author convert(){
+    public Author convert() {
         return new Author(name, email, description);
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
