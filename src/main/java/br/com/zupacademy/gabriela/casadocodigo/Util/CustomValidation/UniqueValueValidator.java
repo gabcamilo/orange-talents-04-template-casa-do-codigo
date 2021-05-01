bb468543a3e785e9217dca6b0854f9885d973c80
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-05-01 18:20:43 -0300

    Changing variable names in UniqueValueValidator

--- src/main/java/br/com/zupacademy/gabriela/casadocodigo/Util/CustomValidation/UniqueValueValidator.java
+++ src/main/java/br/com/zupacademy/gabriela/casadocodigo/Util/CustomValidation/UniqueValueValidator.java
@@ -11,23 +11,23 @@ import java.util.List;
 
 public class UniqueValueValidator implements ConstraintValidator<UniqueValue, Object> {
 
-    private String domainAttribute;
+    private String fieldName;
     private Class<?> aClass;
     @PersistenceContext
     private EntityManager manager;
 
     @Override
     public void initialize(UniqueValue params) {
-        domainAttribute = params.fieldName();
+        fieldName = params.fieldName();
         aClass = params.domainClass();
     }
 
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
-        Query query = manager.createQuery("select 1 from " + aClass.getName() + " where " + domainAttribute + "=:value");
+        Query query = manager.createQuery("select 1 from " + aClass.getName() + " where " + fieldName + "=:value");
         query.setParameter("value", value);
         List<?> list = query.getResultList();
-        Assert.state(list.size() <= 1, "Foi encontrado mais de um " + aClass + "com o atributo " + domainAttribute + " = " + value);
+        Assert.state(list.size() <= 1, "Foi encontrado mais de um " + aClass + "com o atributo " + fieldName + " = " + value);
 
         return list.isEmpty();
     }
